4e5a3fd30fe283c80df8761465050732b6f0f9fb
Evgeniy Kovalev <49534555+ekovalew@users.noreply.github.com>
2020-04-30 11:48:28 +0300

    Update items.py

--- jobparser/items.py
+++ jobparser/items.py
@@ -12,10 +12,10 @@ class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
     _id = scrapy.Field()
     name = scrapy.Field()
+    salary1 = scrapy.Field()
     salary_min = scrapy.Field()
     salary_max = scrapy.Field()
     link = scrapy.Field()
     site = scrapy.Field()
-    salary = scrapy.Field()
 
 
